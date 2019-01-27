66dadff83df327a053060b2b93cf63d57a2fdf93
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-27 16:11:05 -0600

    added a method to create UI

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.mediaplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG          = MainActivity.class.getSimpleName();
+    public static final int    MEDIA_RES_ID = R.raw.music;
+
+    private TextView      textView;
+    private SeekBar       seekBar;
+    private ScrollView    scrollView;
+    private PlayerAdapter playerAdapter;
+    private boolean       userIsSeeking = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        textView = findViewById(R.id.text_debug);
+        Button play = findViewById(R.id.button_play);
+        Button pause = findViewById(R.id.button_pause);
+        Button reset = findViewById(R.id.button_reset);
+        seekBar = findViewById(R.id.seekbar_audio);
+        scrollView = findViewById(R.id.scroll_container);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerAdapter.play();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerAdapter.pause();
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerAdapter.reset();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mediaplayer/MediaPlayerHolder.java
@@ -0,0 +1,40 @@
+package com.example.mediaplayer;
+
+public class MediaPlayerHolder implements PlayerAdapter {
+
+
+    @Override
+    public void loadMedia(int resourceId) {
+
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    @Override
+    public void isPlaying() {
+
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void initializeProgressCallback() {
+
+    }
+}
