4a1b92a5b32fa83bc049b243fd6598d5fe87b2e4
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-27 17:33:51 -0600

    added method to report playback position

--- app/src/main/java/com/example/mediaplayer/MediaPlayerHolder.java
+++ app/src/main/java/com/example/mediaplayer/MediaPlayerHolder.java
@@ -3,6 +3,8 @@ package com.example.mediaplayer;
 import android.content.Context;
 import android.media.MediaPlayer;
 
+import java.util.concurrent.ScheduledExecutorService;
+
 public class MediaPlayerHolder implements PlayerAdapter {
 
     public static final int PLAYBACK_POSITION_REFRESH_INTERVAL_MS = 1000;
@@ -11,6 +13,31 @@ public class MediaPlayerHolder implements PlayerAdapter {
     private MediaPlayer mediaPlayer;
     private int resourceId;
     private PlaybackInfoListener playbackInfoListener;
+    private ScheduledExecutorService executorService;
+    private Runnable seekBarPosition;
+
+    private void initializeMediaPlayer(){
+        if (mediaPlayer == null){
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopUpdatingCallbackWithPosition(true);
+                }
+            });
+        }
+    }
+
+    private void stopUpdatingCallbackWithPosition(boolean resetUIPlaybackPosition) {
+        if (executorService != null){
+            executorService.shutdownNow();
+            executorService = null;
+            seekBarPosition = null;
+            if (resetUIPlaybackPosition && playbackInfoListener != null){
+                playbackInfoListener.onPositionChanged(0);
+            }
+        }
+    }
 
     @Override
     public void loadMedia(int resourceId) {
@@ -23,7 +50,7 @@ public class MediaPlayerHolder implements PlayerAdapter {
     }
 
     public MediaPlayerHolder(Context context) {
-
+        this.context = context.getApplicationContext();
     }
 
     @Override
